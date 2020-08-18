b57aff6c1a1b091381393caef511718738d6a7c2
Karolina Sowinska <32458081+karolina-sowinska@users.noreply.github.com>
2020-08-18 19:06:49 +0100

    Added more comments to the Extract process

--- main.py
+++ main.py
@@ -18,17 +18,19 @@ TOKEN = "" # your Spotify API token
 if __name__ == "__main__":
 
     # Extract part of the ETL process
-    
+ 
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
         "Authorization" : "Bearer {token}".format(token=TOKEN)
     }
-
+    
+    # Convert time to Unix timestamp in miliseconds      
     today = datetime.datetime.now()
     yesterday = today - datetime.timedelta(days=1)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
+    # Download all songs you've listened to "after yesterday", which means in the last 24 hours      
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
     data = r.json()
@@ -38,12 +40,14 @@ if __name__ == "__main__":
     played_at_list = []
     timestamps = []
 
+    # Extracting only the relevant bits of data from the json object      
     for song in data["items"]:
         song_names.append(song["track"]["name"])
         artist_names.append(song["track"]["album"]["artists"][0]["name"])
         played_at_list.append(song["played_at"])
-        timestamps.append(song["played_at"][0:10]) 
-
+        timestamps.append(song["played_at"][0:10])
+        
+    # Prepare a dictionary in order to turn it into a pandas dataframe below       
     song_dict = {
         "song_name" : song_names,
         "artist_name": artist_names,
