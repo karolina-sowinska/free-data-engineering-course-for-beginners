eb82dc4eee977d1e00e6ffd495bbf351816b311f
Karolina Sowinska <karolinasowinska@Karolinas-MacBook-Pro.local>
2020-09-17 21:35:35 +0200

    load stage added

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ import sqlite3
 
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "" # your Spotify username 
-TOKEN = "" # your Spotify API token
+# USER_ID = "" # your Spotify username 
+# TOKEN = "" # your Spotify API token
+USER_ID = "lifewithkarcia" 
+TOKEN = "BQD0dW1jZyaa6EtPKCKQuMjbeK-a_SAc5MdtEA6yshXAPLxJZPJVEbC3fqPInBOeJUrtZVCR6__GPIxVwvatnaUJw_USj8RoQ1pkcaYGtyCsdldDAzt2_qPwViV7AjM6yCI8SOxyU29xoCuUJc4MhMDD1Q3VVQ"
 
 # Generate your token here:  https://developer.spotify.com/console/get-recently-played/
 # Note: You need a Spotify account (can be easily created for free)
@@ -54,7 +56,7 @@ if __name__ == "__main__":
     
     # Convert time to Unix timestamp in miliseconds      
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
+    yesterday = today - datetime.timedelta(days=60)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     # Download all songs you've listened to "after yesterday", which means in the last 24 hours      
@@ -87,6 +89,45 @@ if __name__ == "__main__":
     # Validate
     if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
+
+    # Load
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Opened database successfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database")
+
+    conn.close()
+    print("Close database successfully")
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
     # Job scheduling 
